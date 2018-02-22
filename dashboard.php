e83f09cd6afb419368eca2c69201ac8128b2cdd1
Sangam Adhikari <san.adh2010@gmail.com>
2018-02-22 15:03:00 +0545

    Changed html extension to php

--- dashboard.php
+++ dashboard.php
@@ -17,7 +17,7 @@
 <body>
 	<div class="container-fluid">
       <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="dashboard.html"><img src="img/hero.png" alt="logo"></a>
+  <a class="navbar-brand" href="dashboard.php"><img src="img/hero.png" alt="logo"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation" style="">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -25,7 +25,7 @@
   <div class="collapse navbar-collapse" id="navbarColor01">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="dashboard.html">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="dashboard.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="add.html" >Add</a>
